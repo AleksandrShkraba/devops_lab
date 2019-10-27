315b5e5af8ed8b5b1798ce66cf3d4e39908d8a32
AleksandrShkraba <55788268+AleksandrShkraba@users.noreply.github.com>
2019-10-27 22:37:51 +0300

    Hometask3

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="snapshot",
+    packages=setuptools.find_packages(),
+    version="1.0",
+    author="Aleksandr Shkraba",
+    author_email="Aleksandr_Shkraba@epam.com",
+    description="My first application"
+)
