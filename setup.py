0b3dfa7e6448402cc4a973effea5557946a55d6a
AleksandrShkraba <aleksandrshkraba@epam.com>
2019-10-28 11:31:20 +0300

    correct

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="snapshot",
     packages=setuptools.find_packages(),
-    scripts=[snapshot.py],
+    scripts=['snapshot.py'],
     version="1.0",
     author="Aleksandr Shkraba",
     author_email="Aleksandr_Shkraba@epam.com",
